536a2e07efeaa1aff473779c4fb0902e9fc504d3
gitbike <git_bike_840@yahoo.co.jp>
2019-02-08 17:02:11 +0900

    テストにassertを追加

--- test.js
+++ test.js
@@ -1,3 +1,9 @@
 'use strict';
 const todo = require('./index.js');
+const assert = require('assert');
+
+todo.todo('ノートを買う');
+todo.todo('鉛筆を買う');
+assert.deepEqual(todo.list(), ['ノートを買う', '鉛筆を買う']);
+
 console.log('テストが正常に終了しました');
