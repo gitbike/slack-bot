ba2aa7595f251141c8de867fb348dede9cf8afff
gitbike <git_bike_840@yahoo.co.jp>
2019-02-08 17:47:41 +0900

    doneとdonelistのテストを追加

--- test.js
+++ test.js
@@ -2,8 +2,14 @@
 const todo = require('./index.js');
 const assert = require('assert');
 
+// todoとlistのテスト
 todo.todo('ノートを買う');
 todo.todo('鉛筆を買う');
 assert.deepEqual(todo.list(), ['ノートを買う', '鉛筆を買う']);
+
+// doneとdonelistのテスト
+todo.done('ノートを買う');
+assert.deepEqual(todo.list(), ['鉛筆を買う']);
+assert.deepEqual(todo.donelist(), ['ノートを買う']);
 
 console.log('テストが正常に終了しました');
